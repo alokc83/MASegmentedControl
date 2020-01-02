f45fb3a694154d66be731b7524814b944fd78450
Alok Choudhary <alok.c83@gmail.com>
2020-01-02 11:50:55 -0600

    more correction

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,10 +1,9 @@
-// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(name: "MASegmentedControl",
-                      platforms: [.iOS(.v122)],
+                      platforms: [.iOS(.v13)],
                       products: [.library(name: "MASegmentedControl",
                                           targets: ["MASegmentedControl"])],
                       targets: [.target(name: "MASegmentedControl",
                                         path: "MASegmentedControl")],
-                      swiftLanguageVersions: [.v4, .v4_2, .v5])
+                      swiftLanguageVersions: [.v5])
